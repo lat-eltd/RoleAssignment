e63278aece121e6f23081794c73d8adc2469b430
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-08-09 19:53:49 +0200

    ILIAS 6/7

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = "ldaphk";
 $version = "1.1.0";
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
-$responsible = "Martin Studer, Fabian Schmid, Gabriel Comte";
-$responsible_mail = "info@studer-raimann.ch";
+$ilias_min_version = "6.0";
+$ilias_max_version = "7.999";
+$responsible = "Martin Studer";
+$responsible_mail = "martin@fluxlabs.ch";
